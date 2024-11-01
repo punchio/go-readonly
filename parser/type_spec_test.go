0c1a4f9c250db18ed875fced7ef9bb240969feda
punchio <mail_pwx@163.com>
2024-11-01 18:57:20 +0800

    语法检查规则完善,跨包引入

--- parser/type_spec_test.go
+++ parser/type_spec_test.go
@@ -1,8 +1,10 @@
 package parser
 
 import (
+	"go/ast"
 	"go/parser"
 	"go/token"
+	"go/types"
 	"readonly/tools"
 	"testing"
 )
@@ -22,10 +24,35 @@ func TestCollectTypeInfo(t *testing.T) {
 	// 创建类型检查器
 
 	fset := token.NewFileSet()
+
 	pkgs, err := parser.ParseDir(fset, "./testdata", nil, parser.AllErrors)
 	if err != nil {
 		panic(err)
 	}
+
+	files := make([]*ast.File, 0)
+	for _, pkg := range pkgs {
+		for _, file := range pkg.Files {
+			files = append(files, file)
+		}
+	}
+
+	conf := types.Config{Error: func(err error) {
+		return
+	}}
+	info := &types.Info{
+		Types:        make(map[ast.Expr]types.TypeAndValue),
+		Instances:    make(map[*ast.Ident]types.Instance),
+		Defs:         make(map[*ast.Ident]types.Object),
+		Uses:         make(map[*ast.Ident]types.Object),
+		Implicits:    map[ast.Node]types.Object{},
+		Scopes:       map[ast.Node]*types.Scope{},
+		Selections:   map[*ast.SelectorExpr]*types.Selection{},
+		InitOrder:    []*types.Initializer{},
+		FileVersions: map[*ast.File]string{},
+	}
+	pkg, err := conf.Check("./testdata", fset, files, info)
+	_ = pkg
 
 	CollectTypeSpec(pkgs)
 	CheckReadonly(fset)
